3846679cab58bfbd96ecbe486ee404ee17891549
mmegarief <38590563+mmegarief@users.noreply.github.com>
2024-12-07 14:02:08 -0800

    Fix 1 main.tf

--- main.tf
+++ main.tf
@@ -14,11 +14,8 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-<<<<<<< HEAD
-resource "aws_instance"web" {
-=======
 resource "aws_instance" "web" {
->>>>>>> 9a004b4ce052958f3c64aed463e2d008b47b4fbc
+
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
