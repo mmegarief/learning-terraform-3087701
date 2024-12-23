023890735e5194b4ff8a13b195d376b05c7659ec
mmegarief <38590563+mmegarief@users.noreply.github.com>
2024-12-23 09:18:10 -0800

    create new dev vpc

--- main.tf
+++ main.tf
@@ -14,11 +14,50 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
+data "aws_vpc" "default" {
+  default = true  
+}
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  # private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"] # in the course example this was deleted to kept only public subnets
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  # enable_nat_gateway = true # won't be using this
+  # enable_vpn_gateway = true # won't be using this
 
   tags = {
-    Name = "HelloWorld"
+    Terraform = "true"
+    Environment = "dev"
   }
+}
+
+resource "aws_instance" "blog" {
+  ami                             = data.aws_ami.app_ami.id
+  instance_type                   = var.instance_type
+  vpc_security_group_ids          = [module.blog_sg.vpc_security_group_ids]
+
+  subnet_id = module.blog.vpc.public_subnets[0]
+
+  tags = {
+    Name = "Learning Terraform"
+    # Name = "HelloWorld" 
+  }
+}
+
+modeule "blog_sg" {
+  source = "terraform-aws-modules/security-vpc_security_group/aws"
+  version ="4.13.0"
+
+  vpc_id =data.blog_vpc_vpc_id
+  name   ="blog"
+  ingress_rules = ["http-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = [0.0.0.0/0]
 }
